f0cd2d9bda932de2d315e0504488e73631ae506d
Micael Makenzius <112689339+sweco-seiamm@users.noreply.github.com>
2025-11-12 13:41:51 +0100

    refactor for globe plugin and request handling changes

--- tasks/webpack.dev.js
+++ tasks/webpack.dev.js
@@ -9,31 +9,23 @@ module.exports = merge(common, {
     library: {
       type: 'var',
       export: 'default',
-      name: 'Origo'
-    }
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, '../') // Correct directory path
+      name: 'Origo',
     },
-    port: 9966,
-    setupMiddlewares: (middlewares, devServer) => {
-      devServer.app.get('*.terrain', function(req, res, next) {
-        console.log('Requesting:', req.url);  // Log request URL for debugging
-        res.setHeader('Content-Type', 'application/octet-stream'); // safe default
-        // res.setHeader('Content-Encoding', 'gzip'); //  keep this if your terrain tiles are stored gzipped (they usually are)
-        res.setHeader('Cache-Control', 'public, max-age=2592000'); // 30 days
-        next();
-      });
-      devServer.app.get('*.glb', function(req, res, next) {
-        console.log('Requesting:', req.url);  // Log request URL for debugging
-        res.setHeader('Content-Type', 'model/gltf-binary');
-        res.setHeader('Cache-Control', 'public, max-age=2592000');
-        next();
-      });
-      return middlewares;
-    }
   },
   devtool: 'eval-cheap-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '../'),
+      watch: {
+        ignored: ['**/data/**'],
+      },
+      staticOptions: {
+        headers: {
+          'Cache-Control': 'public, max-age=2592000',
+        },
+      },
+    },
+    compress: true,
+    port: 9966,
+  },
 });
-
